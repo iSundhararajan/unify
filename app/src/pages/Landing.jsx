b028d2c364ef0ca9624ba8b085336f2f1e6ef916
Ishwarya Sundhararajan <80136488+iSundhararajan@users.noreply.github.com>
2022-10-23 00:14:36 -0400

    Update Landing.jsx

--- app/src/pages/Landing.jsx
+++ app/src/pages/Landing.jsx
@@ -1,23 +1,69 @@
-import logo from '../logo.svg';
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 
+import { useNavigate } from 'react-router-dom';
 
 export default function Landing() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  let navigate = useNavigate();
+  const routeDashboard = () => {
+    navigate(`link`);
+  };
+
+  return (
+    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+      <Flex p={8} flex={1} align={'center'} justify={'center'}>
+        <Stack spacing={6} w={'full'} maxW={'lg'}>
+          <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+            <Text
+              as={'span'}
+              position={'relative'}
+              _after={{
+                content: "''",
+                width: 'full',
+                height: useBreakpointValue({ base: '20%', md: '30%' }),
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                zIndex: -1,
+              }}>
+              Unify
+            </Text>
+            <br />{' '}
+            
+          </Heading>
+          <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+            Connect with people around the world and help combat social issues.
+          </Text>
+          <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+            <Button
+              rounded={'full'}
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{
+                bg: 'blue.500',
+              }}>
+              Join Us
+            </Button>
+            <Button rounded={'full'}>How It Works</Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={'Login Image'}
+          objectFit={'cover'}
+          src={
+            'https://imageio.forbes.com/specials-images/imageserve/6182f894b7308dbf73e42480/0x0.jpg?format=jpg&width=1200'
+          }
+        />
+      </Flex>
+    </Stack>
+  );
 }
